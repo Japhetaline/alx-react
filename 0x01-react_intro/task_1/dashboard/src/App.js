39857a84691e29e077804b10afa2ba1a3aa56443
Japhetaline <Japhetdamassoh@gmail.com>
2024-01-06 14:37:25 +0100

    react-intro

--- 0x01-react_intro/task_1/dashboard/src/App.js
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -1,17 +1,20 @@
-import holberton_logo from "./holberton_logo.jpg";
+import logo from "./holberton-logo.jpg";
 import "./App.css";
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-        <img src={holberton_logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-      <div className="App-footer">Copyright 2020 - holberton School</div>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
     </div>
   );
 }
